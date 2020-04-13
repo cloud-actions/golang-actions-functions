d6171aaba983173ba7eb0342ac91b513c6f9879d
Aaron Wislang <aaron.wislang@microsoft.com>
2020-04-12 20:21:06 -0400

    align windows and linux scripts

--- azure/DEPLOY-WINDOWS.sh
+++ azure/DEPLOY-WINDOWS.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # variables
-RESOURCE_GROUP='200200-hello-gopher'
+RESOURCE_GROUP='200300-hello-gopher-win'
 LOCATION='eastus'
 SUBSCRIPTION_ID=$(az account show | jq -r .id)
 SCOPE="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}"
@@ -11,8 +11,10 @@ RANDOM_STR=$(echo -n "$SCOPE" | shasum | head -c 6)
 STORAGE_NAME="storage${RANDOM_STR}"
 FUNCTION_NAME="functions${RANDOM_STR}"
 CREATE_IF_EXISTS="false"
+# set by actions workflow
+[[ -z "${GITHUB_SHA:-}" ]] && GITHUB_SHA='test'
 
-echo "RANDOM_STR: {$RANDOM_STR}"
+echo "RANDOM_STR: ${RANDOM_STR}"
 
 TMP=$(az storage account list -g $RESOURCE_GROUP | jq '[.[].name | index("'$STORAGE_NAME'")] | length')
 
@@ -39,16 +41,15 @@ if [[ "$TMP" == "0" || $CREATE_IF_EXISTS == "true" ]]; then
     echo "az functionapp appsettings..."
     az functionapp config appsettings set -g $RESOURCE_GROUP -n $FUNCTION_NAME --settings \
         "FUNCTIONS_EXTENSION_VERSION=~3" \
-        "SERVER_NAME=hello-gopher-${GITHUB_SHA}" \
-        > /dev/null
-
-    echo "az functionapp config..."
-    az functionapp config set -g $RESOURCE_GROUP -n $FUNCTION_NAME \
-        --use-32bit-worker-process false \
         > /dev/null
 else
     echo "functionapp exists..."
 fi
+
+echo "az functionapp appsettings (SERVER_NAME)..."
+az functionapp config appsettings set -g $RESOURCE_GROUP -n $FUNCTION_NAME --settings \
+    "SERVER_NAME=hello-gopher-${GITHUB_SHA}" \
+    > /dev/null
 
 echo "build binary..."
 source build-container-windows.sh
@@ -58,4 +59,4 @@ cp host.windows.json host.json
 source deploy-storage.sh
 
 echo "curl https://${FUNCTION_NAME}.azurewebsites.net/api/healthz"
-curl -s -w '%{time_starttransfer}\n' "https://${FUNCTION_NAME}.azurewebsites.net/api/healthz"
+curl -s -w '\ntime_starttransfer: %{time_starttransfer}\n' "https://${FUNCTION_NAME}.azurewebsites.net/api/healthz"
